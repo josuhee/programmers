8a5a79f1e97db793cb1cb89c2225131178a63b46
josuhee <suheeqwe@gmail.com>
2021-09-02 20:55:11 +0900

    직사각형 별찍기

---
+++ code/level1/level1_8.cpp
@@ -0,0 +1,20 @@
+//연습문제
+//직사각형 별찍기
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    for (int i = 1; i <= b; i++)
+    {
+        for (int j = 1; j <= a; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
