48001a01018795346ae5d17e979968dee0b87b3d
josuhee <suheeqwe@gmail.com>
2021-09-30 09:37:36 +0900

    2개 이하로 다른 비트

---
+++ code/level2/level2_6.cpp
@@ -0,0 +1,26 @@
+//월간 코드 챌린지 시즌2
+//2개 이하로 다른 비트
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    int size = numbers.size();
+    
+    for (int tc = 0; tc < size; tc++)
+    {
+        if (numbers[tc] % 2 == 0)
+            answer.push_back(numbers[tc] + 1);
+        else
+            answer.push_back(numbers[tc] + ((numbers[tc] ^ (numbers[tc] + 1)) + 1) / 4);
+    }
+    return answer;
+}
+
+/*
+숫자의 최상위 비트 구하기
+num + ((num ^ (num + 1)) + 1) / 4
+*/
